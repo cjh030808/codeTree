3c97d512ab076b39a7c9215892e6d2ffab1ae107
cjh030808 <65361625+cjh030808@users.noreply.github.com>
2024-12-23 11:41:17 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241223/2021년 날짜의 유무/with-or-without-2021.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int monthDayCalculator(int M, int D){
+    int monthDay;
+    switch(M) {
+        case 1:  // 1월
+        case 3:  // 3월
+        case 5:  // 5월
+        case 7:  // 7월
+        case 8:  // 8월
+        case 10: // 10월
+        case 12: // 12월
+            monthDay = 31;
+            break;
+        case 4:  // 4월
+        case 6:  // 6월
+        case 9:  // 9월
+        case 11: // 11월
+            monthDay = 30;
+            break;
+        case 2:  // 2월
+            monthDay = 28;
+            break;
+    }
+    if(monthDay < D) return 0;
+    return 1;
+}
+
+int main() {
+    int M, D;
+    scanf("%d %d", &M, &D);
+
+    if(monthDayCalculator) printf("Yes");
+    else printf("No");
+    return 0;
+}
