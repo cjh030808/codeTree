14e6f23b081ee621b68c54e8c9f0a53c35318fe2
cjh030808 <65361625+cjh030808@users.noreply.github.com>
2024-12-24 23:28:56 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241224/그 계절, 그 날/that-season-that-day.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int leapYearCheck(int year){
+    if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+        return 1;
+    } 
+    return 0;
+}
+
+int dayValidCheck(int year, int month, int day){
+    int monthDay;
+    if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+        monthDay = 31;
+    } else if(month == 4 || month == 6 || month == 9 || month == 11){
+        monthDay = 30;
+    } else if(month == 2){
+        if(leapYearCheck(year)) monthDay= 29;
+        else monthDay = 28;
+    }
+    if(monthDay < day) return 0;
+    return 1;
+}
+
+int seasonCheck(int month){
+    if(3 <= month  && month <= 5) return 1;
+    else if(6 <= month && month <= 8) return 2;
+    else if(9 <= month && month <= 11) return 3;
+    else return 4;
+}
+
+int main() {
+    int Y, M, D;
+    int season;
+    scanf("%d %d %d", &Y, &M, &D);
+    if(dayValidCheck(Y, M, D)){
+        season = seasonCheck(M);
+    } else printf("-1");
+
+    switch(season) {
+        case 1 :
+            printf("Spring");
+            break;
+        case 2 :
+            printf("Summer");
+            break;
+        case 3 :
+            printf("Fall");
+            break;
+        case 4 :
+            printf("Winter");
+            break;
+        default:
+            printf("-1");
+            break;
+    }
+    return 0;
+}
