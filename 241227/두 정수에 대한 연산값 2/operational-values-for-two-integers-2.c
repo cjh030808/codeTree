ed47f9a0c47d84a97edf043b26becb64b2efdc22
cjh030808 <65361625+cjh030808@users.noreply.github.com>
2024-12-27 22:21:21 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241227/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void plusTenMultTwo(int *a, int *b){
+    if(*a < *b) {
+        *a += 10;
+        *b *= 2;
+    } else {
+        *a *= 2;
+        b += 10;
+    }
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    plusTenMultTwo(&a, &b);
+    printf("%d %d", a, b);
+    return 0;
+}
