0f0f258b3838f6177079ac79b749bcc0b851383b
cjh030808 <65361625+cjh030808@users.noreply.github.com>
2024-12-30 23:32:10 +0900

    Title: 반복 출력하기 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241230/반복 출력하기 2/repeated-output-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void recursiveFunc(int n){
+    if(n == 0) return;
+
+    recursiveFunc(n - 1);
+    printf("HelloWorld\n");
+}
+
+int main() {
+    int n ;
+    scanf("%d", &n);
+    recursiveFunc(4);
+    return 0;
+}
