8433a470f9c6d92c3c23039bd9757b7056e30621
cjh030808 <65361625+cjh030808@users.noreply.github.com>
2025-01-02 12:07:49 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250102/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.c
+++ 250102/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-int sumByEven(int n) {
-    if(n == 0) return 0;
-    return sumByEven(n - 2) + n;
-}
-
-int sumByOdd(int n) {
+int sumByNum(int n) {
     if(n == 1) return 1;
-    return sumByOdd(n - 2) + n;
+    if(n == 2) return 2;
+    return sumByNum(n - 2) + n;
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    if(n % 2 == 0) printf("%d", sumByEven(n));
-    else printf("%d", sumByOdd(n));
+    printf("%d", sumByNum(n));
     return 0;
 }
